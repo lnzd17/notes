c676820c739b82910afe2edb7dc394b05dd70e6e
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-06-07 16:34:03 +0000

    added create functionality

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe NotesController, type: :controller do
     end
   end
 
+  describe 'notes#create action' do
+    before(:each) do
+      post :create, params: {note: {title: 'First', content: 'Hello'} }
+    end
+
+    it 'returns 200 status-code' do
+      expect(response).to be_success
+    end
+
+    it 'saves a note to the database' do
+      note = Note.last
+      expect(note.title).to eq('First')
+      expect(note.content).to eq('Hello')
+    end
+
+    it 'returns the note in response body' do
+      json = JSON.parse(response.body)
+      expect(json['title']).to eq('First')
+      expect(json['content']).to eq('Hello')
+    end
+  end
 end
